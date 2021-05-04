fcb377d2cf9bf447d306ae05ce7bea54485d41aa
Aswin Remesan Achu <aswinremesan@gmail.com>
2021-05-04 19:51:41 +0530

    entire app

---
+++ firebase.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import 'firebase/storage'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDAON0vSnaYL6SE6SOcx6hW_2--mg4hjeg",
+  authDomain: "whatsapp-2-242ee.firebaseapp.com",
+  projectId: "whatsapp-2-242ee",
+  storageBucket: "whatsapp-2-242ee.appspot.com",
+  messagingSenderId: "524807876312",
+  appId: "1:524807876312:web:990bdebb7e51fbcd78b1ad"
+};
+
+  const app = !firebase.apps.length 
+    ? firebase.initializeApp(firebaseConfig)
+    : firebase.app();
+
+
+    const db = app.firestore();
+    const auth = app.auth();
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+
+
+    export { db, auth, provider };
